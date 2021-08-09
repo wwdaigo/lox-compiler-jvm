c735df3844a1ebb94d5972b97b850648ad17555d
Daigo Matsuoka <daigo.matsuoka@navvis.com>
2021-08-10 00:21:03 +0200

    Token structure

---
+++ src/main/kotlin/Token.kt
@@ -0,0 +1,32 @@
+class Token(
+	private val type: TokenType,
+	private val lexeme: String,
+	private val literal: Any,
+	private val line: Int) {
+
+	override fun toString(): String {
+		return "$type $lexeme $literal"
+	}
+}
+
+/*
+package com.craftinginterpreters.lox;
+
+class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line;
+
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
+}
+ */
